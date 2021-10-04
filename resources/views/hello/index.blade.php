a532fe813d7d740ff08ab9b5a627948ec0a3455f
kaibonjin <kaibonjin@gmail.com>
2021-10-04 15:38:53 +0900

    ベースレイアウト、継承レイアウト作成

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -1,46 +1,16 @@
-<html>
+@extends('layouts.helloapp')
+@section('title', 'Index')
 
-<head>
-    <title>Hello</title>
-    <style>
-        body {
-            font-size: 16pt;
-            color: #999;
-        }
+@section('menubar')
+    @parent
+    インデックスページ
+@endsection
 
-        h1 {
-            font-size: 100pt;
-            text-align: right;
-            color: #eee;
-            margin: -40px 0px -50px 0px;
-        }
-    </style>
-</head>
+@section ('content')
+<p>ここが本文のコンテンツです</p>
+<p>必要なだけ記述できます</p>
+@endsection
 
-<body>
-    <h1>Blade/Index</h1>
-    <p>&#064;whileの例</p>
-    <ol>
-        @php
-        $counter = 0;
-        @endphp
-        @while ($counter < count($data))
-        <li>{{$data[$counter]}}</li>
-        @php
-        $counter++;
-        @endphp
-        @endwhile
-    </ol>
-    {{-- @isset ($msg)
-    <p>こんにちは、{{$msg}}さん。</p>
-    @else
-    <p>名前を入力してください</p>
-    @endisset
-    <form method="POST" action="/hello">
-        @csrf
-        <input type="text" name="msg">
-        <input type="submit">
-    </form> --}}
-</body>
-
-</html>
+@section('footer')
+copyright 2021 kaisei.
+@endsection

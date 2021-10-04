0a4261912c85a720aa66662b197dacce04dda051
kaibonjin <kaibonjin@gmail.com>
2021-10-04 18:34:50 +0900

    Service Providerを使って

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -10,12 +10,12 @@ class HelloController extends Controller
 {
     public function index ()
     {
-        $data = [
-            ['name'=>'山田太郎', 'mail'=>'taro@yamada'],
-            ['name'=>'小山', 'mail'=>'taro@こやま'],
-            ['name'=>'スズキ', 'mail'=>'taro@yaa'],
-        ];
-        return view('hello.index', ['data'=>$data]);
+        // $data = [
+        //     ['name'=>'山田太郎', 'mail'=>'taro@yamada'],
+        //     ['name'=>'小山', 'mail'=>'taro@こやま'],
+        //     ['name'=>'スズキ', 'mail'=>'taro@yaa'],
+        // ];
+        return view('hello.index', ['message'=>'Hello!']);
     }
     public function post (Request $request)
     {

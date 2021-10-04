c2fbd018357001b94f8cb1ddaade9972da7d0604
kaibonjin <kaibonjin@gmail.com>
2021-10-04 16:07:47 +0900

    コンポーネント、サブビュー追加

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -9,6 +9,7 @@
 @section ('content')
 <p>ここが本文のコンテンツです</p>
 <p>必要なだけ記述できます</p>
+    @include('components.message', ['msg_title' => 'OK', 'msg_content' => 'サブビューです'])
 @endsection
 
 @section('footer')

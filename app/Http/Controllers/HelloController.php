2deb6b72f1e09554125ee685caf79fb37a325afe
kaibonjin <kaibonjin@gmail.com>
2021-10-11 18:04:01 +0900

    パラメータ結合（idが指定されている場合、そのidを取得する)

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -13,43 +13,15 @@ class HelloController extends Controller
 {
     public function index (Request $request)
     {
-        // $validator = Validator::make($request->query(), ['id'=>'required', 'pass' => 'required']);
-        // if ($validator->fails()) {
-        //     $msg = 'クエリーに問題があります';
-        // } else {
-        //     $msg = 'ID/PASSを受付ました。フォームを入力してください';
-        // }
-            //クッキー
-        $items = DB::select('select * from people');
-        return view('hello.index', ['items'=>$items]);
+        if (isset($request->id))
+        {
+            $param = ['id' => $request->id];
+            $items = DB::select('select * from people where id = :id',$param);
+        } else {
+            $items = DB::select('select * from people');
+        }
+        return view('hello.index', ['items' => $items]);
     }
-
-    // public function post (Request $request)
-    // {
-    //     $rules = [
-    //         'name' => 'required',
-    //         'mail' => 'email',
-    //         'age' => 'numeric',
-    //     ];
-    //     $messages = [
-    //         'name.required' => '名前は必ず入力してください',
-    //         'mail.required' => 'メールアドレスが必要です',
-    //         'age.numeric' => '年齢は整数で記入してください',
-    //         'age.min'=> '年齢はゼロ歳以上で記入ください',
-    //         'age.max'=> '年齢は200歳以下で記入ください',
-    //     ];
-
-    //     $validator = Validator::make($request->all(), $rules, $messages);
-
-    //     $validator->sometimes('age', 'min:0', function($input){return !is_int($input->age);});
-
-    //     $validator->sometimes('age', 'max:200', function($input){return !is_int($input->age);});
-
-    //     if ($validator->fails()) {
-    //         return redirect('/hello')->withErrors($validator)->withInput();
-    //     }
-    //     return view('hello.index', ['msg'=>'正しく入力されました']);
-    // }
     public function post (Request $request)
     {
         $validate_rule = ['msg' => 'required',];

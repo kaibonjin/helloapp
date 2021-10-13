de2048bdc1cb355603864924c9bc4ec1ba74180d
kaibonjin <kaibonjin@gmail.com>
2021-10-13 17:53:47 +0900

    add,createメソッド追加

--- app/Http/Controllers/BoardController.php
+++ app/Http/Controllers/BoardController.php
@@ -11,3 +11,17 @@ class BoardController extends Controller
         $items = Board::all();
         return view('board.index', ['items' => $items]);
     }
+    public function add(Request $request)
+    {
+        return view('board.add');
+    }
+    public function create(Request $request)
+    {
+        $this->validate($request, Board::$rules);
+        $board = new Board;
+        $form = $request->all();
+        unset($form['_token']);
+        $board->fill($form)->save();
+        return redirect('/board');
+    }
+}

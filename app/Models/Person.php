b29355255e15e9f2358fe1ff0964d367ac70d7ec
kaibonjin <kaibonjin@gmail.com>
2021-10-13 15:34:44 +0900

    モデルにルール追加

--- app/Models/Person.php
+++ app/Models/Person.php
@@ -11,29 +11,14 @@ class Person extends Model
 {
     use HasFactory;
 
+    protected $guarded = array('id');
+
+    public static $rules = array(
+        'name' => 'required', 'mail' => 'email', 'age' => 'integer|min:0|max:150'
+    );
+
     public function getData()
     {
         return $this->id . ': ' . $this->name . ' (' . $this->age . ')';
-    }
-
-    public function scopeNameEqual($query, $str)
-    {
-        return $query->where('name', $str);
-    }
-
-    public function scopeAgeGreaterThan($query, $int)
-    {
-        return $query->where('age', '>=', $int);
-    }
-    public function scopeAgeLessThan($query, $int)
-    {
-        return $query->where('age', '<=', $int);
-    }
-
-    protected static function boot()
-    {
-        parent::boot();
-
-        static::addGlobalScope(new ScopePerson);
     }
 }

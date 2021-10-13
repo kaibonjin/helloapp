e891e5869fb118fc98554e1f8c692ecca353f2a6
kaibonjin <kaibonjin@gmail.com>
2021-10-13 16:04:19 +0900

    修正

--- resources/views/person/add.blade.php
+++ resources/views/person/add.blade.php
@@ -12,8 +12,8 @@
 @if(count($errors) > 0)
 <div>
     <ul>
-        @foreach ($errors as $item)
-            <li>{{ $item }}</li>
+        @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
         @endforeach
     </ul>
 </div>
@@ -23,7 +23,7 @@
             @csrf
             <tr><th>name: </th><td><input type="text" name="name" value="{{old('name')}}"></td></tr>
             <tr><th>mail: </th><td><input type="text" name="mail" value="{{old('mail')}}"></td></tr>
-            <tr><th>age: </th><td><input type="text" name="name" value="{{old('age')}}"></td></tr>
+            <tr><th>age: </th><td><input type="text" name="age" value="{{old('age')}}"></td></tr>
             <tr><th></th><td><input type="submit" value="submit"></td></tr>
         </table>
     </form>

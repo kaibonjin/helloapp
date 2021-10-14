02b1f20af9bd0190df726f20aa7a8882c11b7cb7
kaibonjin <kaibonjin@gmail.com>
2021-10-14 11:59:43 +0900

    seeder記述

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,5 +15,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(PeopleTableSeeder::class);
         $this->call(ProductTableSeeder::class);
+        $this->call(RestdataTableSeeder::class);
     }
 }
